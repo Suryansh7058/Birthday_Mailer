7676cad1b419bb5e90119e273dcc4df39931b715
Suryansh7058 <83452887+Suryansh7058@users.noreply.github.com>
2022-03-05 01:44:16 +0530

    Complete JSON access

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const nodemailer = require('nodemailer');
 const cron = require('node-cron');
 require('dotenv').config();
+const collegeStaff = require('./College_Staff.json');
 
 let transporter = nodemailer.createTransport({
   service: 'gmail',
@@ -10,12 +11,51 @@ let transporter = nodemailer.createTransport({
   },
 });
 
+const birthday = () => {
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0');
+
+  let staffDetails = '';
+  collegeStaff.forEach((staff) => {
+    if (staff.DOB.substring(3, 5) === mm && staff.DOB.substring(0, 2) === dd) {
+      staffDetails += `Name: ${staff.Name}\nJob Role: ${staff.JobRole}\nCollege: ${staff.institution}\nBranch: ${staff.department}\nContact Number: ${staff.MobileNumber}\n\n\n`;
+    }
+  });
+  return staffDetails;
+};
+birthday();
+
+let monthNames = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+
+const getDate = () => {
+  let today = new Date();
+  let dd = String(today.getDate()).padStart(2, '0');
+  let mm = String(today.getMonth() + 1).padStart(2, '0');
+
+  return `${dd} - ${monthNames[today.getMonth()]} - ${today.getFullYear()}`;
+};
+
 let mailOptions = {
   from: process.env.EMAIL,
   to: 'Suryanshsingh7058@outlook.com',
   subject: "Email from Suryansh's Mail App ",
-  text: 'Testing Mail Scheduling',
+  text: `Staff With Birthday on ${getDate()} :\n\n\n${birthday()}`,
 };
+
 
 cron.schedule('0 0 0 * * *', () => {
   transporter.sendMail(mailOptions, function (error, info) {
@@ -26,3 +66,4 @@ cron.schedule('0 0 0 * * *', () => {
     }
   });
 });
+
