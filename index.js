10a0b7075615429d51d2c4b7a28c17ffc08c6253
Suryansh7058 <83452887+Suryansh7058@users.noreply.github.com>
2022-09-06 17:19:47 +0530

    Enabled More Secure apps in the google account

--- index.js
+++ index.js
@@ -17,14 +17,15 @@ const birthday = () => {
   let mm = String(today.getMonth() + 1).padStart(2, '0');
 
   let staffDetails = '';
+
+  // Date Format in JSON dd/mm/yyyy
   collegeStaff.forEach((staff) => {
     if (staff.DOB.substring(3, 5) === mm && staff.DOB.substring(0, 2) === dd) {
-      staffDetails += `Name: ${staff.Name}\nJob Role: ${staff.JobRole}\nCollege: ${staff.institution}\nBranch: ${staff.department}\nContact Number: ${staff.MobileNumber}\n\n`;
+      staffDetails += `Name: ${staff.Name}\nJob Role: ${staff.JobRole}\nCollege: ${staff.institution}\nBranch: ${staff.department}\nContact Number: ${staff.MobileNumber}\nEmail ID: ${staff.email} \n\n`;
     }
   });
-  return staffDetails;
+  return staffDetails.length > 0 ? staffDetails : 'No Birthdays Today';
 };
-birthday();
 
 let monthNames = [
   'January',
@@ -48,20 +49,27 @@ const getDate = () => {
 
   return `${dd} - ${monthNames[today.getMonth()]} - ${today.getFullYear()}`;
 };
-// amitajajoo@gmail.com
+// suryanshsingh989@gmail.com, vivekchourey60@gmail.com ,mrsrashmisrivastava@gmail.com ,
+//  aajajoo@dypcoeakurdi.ac.in , hhpatel@dypcoeakurdi.ac.in
 let mailOptions = {
   from: process.env.EMAIL,
-  to: 'Suryanshsingh7058@outlook.com',
+  to: 'suryanshsingh989@gmail.com, vivekchourey60@gmail.com',
   subject: `Staff With Birthday on ${getDate()}`,
   text: birthday(),
 };
-
-cron.schedule('0 0 0 * * *', () => {
-  transporter.sendMail(mailOptions, function (error, info) {
-    if (error) {
-      console.log(error);
-    } else {
-      console.log('Email sent: ' + info.response);
-    }
-  });
+// cron.schedule('0 0 0 * * *', () => {
+//   transporter.sendMail(mailOptions, function (error, info) {
+//     if (error) {
+//       console.log(error);
+//     } else {
+//       console.log('Email sent: ' + info.response);
+//     }
+//   });
+// });
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('Email sent: ' + info.response);
+  }
 });
